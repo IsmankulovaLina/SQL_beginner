a977289016130b845e2e71a6446b0b377fa7558d
IsmankulovaLina <ismankulovalina@gmail.com>
2025-07-29 17:46:47 +0300

    Add files via upload

---
+++ src/Day06/day06_ex05.sql
@@ -0,0 +1,5 @@
+COMMENT ON TABLE person_discounts IS 'Persons discounts';
+COMMENT ON COLUMN person_discounts.id IS 'Primary Key Identifier';
+COMMENT ON COLUMN person_discounts.person_id IS 'Person Identifier';
+COMMENT ON COLUMN person_discounts.pizzeria_id IS 'Pizzeria Identifier';
+COMMENT ON COLUMN person_discounts.discount IS 'Discount Amount';
